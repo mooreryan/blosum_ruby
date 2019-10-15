46398bbcce2d24dc44709bfad3713e929f356bb4
Ryan Moore <moorer@udel.edu>
2019-10-15 18:06:12 -0400

    Initial commit

---
+++ lib/blosum.rb
@@ -0,0 +1,6 @@
+require "blosum/version"
+
+module Blosum
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/blosum_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Blosum do
+  it "has a version number" do
+    expect(Blosum::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
